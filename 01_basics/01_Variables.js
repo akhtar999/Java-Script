f97de47fef4143f31008649f6e13f0b5b544d092
Akhtarul Islam <78856199+akhtar999@users.noreply.github.com>
2023-10-17 17:50:47 +0000

    added one extention

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 1223
+let accountEmail = 'akhtar@gmail.com' // a memory will create and later we can chnge the value also
+var accountName = 'Akhtar'
+accountCity = 'Guwahati'
+
+// accountId = 4 // not allowed cause it is const 
+
+accountEmail = 'akhtarul@gmail.com'
+accountName = 'yo yo'
+accountCity = 'assam'
+
+console.table([accountId, accountEmail, accountName, accountCity]);
