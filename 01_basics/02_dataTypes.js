ac440dfd339559931a17489e5ac92e2b56279051
Akhtarul Islam <78856199+akhtar999@users.noreply.github.com>
2023-10-18 07:03:33 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all code of JS as newer version
+
+// alert(3 + 3); it will not work cause we are using node js, not browser
+let age = 18  // int
+let isLoggedIn = true // true ir false 
+let name ="Akhtar" // string
+let place; // undefined, value is not defined yet
+let birth = null // standalone value
+// symbol = unique  
+
+// BigInt it also a datatype use in large number
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);   // object
+
