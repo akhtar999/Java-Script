edd60af3eec21d93af4756cea71450f26df1ea21
Akhtarul Islam <78856199+akhtar999@users.noreply.github.com>
2023-10-20 09:16:13 +0000

    Coversion Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,9 +3,9 @@ let score = null; // try with true/undefined/null/string
 // console.log(typeof score);
 // console.log(typeof (score));
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score);
 // console.log(typeof valueInNumber);
-// console.log(valueInNumber); 
+// console.log(valueInNumber);
 
 //Conversion to number
 
@@ -17,9 +17,8 @@ let valueInNumber = Number(score)
 // undefined => NaN
 // null => 0
 
-
-let isLoggedIn = undefined
-let BooleanIsLoggedIn = Boolean(isLoggedIn)
+let isLoggedIn = undefined;
+let BooleanIsLoggedIn = Boolean(isLoggedIn);
 
 // console.log(BooleanIsLoggedIn);
 
@@ -33,8 +32,39 @@ let BooleanIsLoggedIn = Boolean(isLoggedIn)
 // null => false
 // undefined => false
 
-let aNumber = 46
-let stringNumber = String(aNumber)
+let aNumber = 46;
+let stringNumber = String(aNumber);
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// ********************************************Operations**********************************************
+
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(3+3);
+// console.log(3*3);
+// console.log(3**3);
+// console.log(9/3);
+// console.log(8%3);
+
+let str1 = "Hello";
+let str2 = " Akhtar";
+
+// let str3 = str1 + str2; 
+// console.log(str3); => hello Akhtar
+
+// console.log(1 + "2"); => 12
+// console.log("1" + 2); => 12
+// console.log(1 + 2 + "2"); => 32
+// console.log("1" + 2 + 2); => 122
+
+
+
+let gameCounter = 100;
+const x = ++gameCounter;
+// ++gameCounter;
+console.log(gameCounter, x);
+
