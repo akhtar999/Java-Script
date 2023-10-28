9ec5860a2cbb8061975a17dd3450a40575fd8b82
Akhtar <iaktr999@gmail.com>
2023-10-28 22:45:14 +0530

    objects

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,46 @@
+// singleton
+// Object.create
+
+//object literals
+
+const mySym = Symbol("key1"); // interview : how to add symbol in object and access it ?
+
+const user = {
+  name: "akhtar",
+  [mySym]: "key2",
+  "full name": "akhtarul islam", // we cant access this by dot notation
+  age: 23,
+  email: "akhtar@gmail.com",
+  pin: 784146,
+  isLoggedIn: true,
+};
+
+//behind the scene key are treated as string. ex {name: "akhtar"} ---> {"name": "akhtar"}
+// console.log(user.email); // akhtar@gmail.com
+// console.log(user["email"]); // akhtar@gmail.com
+// console.log(user["full name"]); // akhtarul islam
+// console.log(user[mySym]); // key2
+
+//we can manipulate values of object
+
+user.email = "akhtar999@gmail.com";
+// console.log(user);
+
+//we can freeze object (after changes will not apply)
+// Object.freeze(user);
+
+user.email = "akhtar888@gmail.com"; // it will not change
+
+// console.log(user);
+
+user.greeting = function () {
+  console.log("good moorning");
+};
+
+user.greetingTwo = function () {
+  console.log(`hi Aspire, ${this.name}`);
+};
+
+console.log(user.greeting());
+console.log(user.greetingTwo());
+console.log(user);
